9895a3afa5de4e19c909f47a1c3827b0f76a97d1
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-09-07 09:13:58 +0800

    Initial commit

---
+++ src/motioninput_tui/__init__.py
@@ -0,0 +1,9 @@
+"""Python App motioninput-tui."""
+
+from .constants import PROGRAM_NAME, PROGRAM_REPO_URL, PROGRAM_VERSION
+
+__all__ = [
+    "PROGRAM_NAME",
+    "PROGRAM_REPO_URL",
+    "PROGRAM_VERSION",
+]
